1d522947a60c584a883117c75673beef708b2e24
jy2054050 <jy2054050@gmail.com>
2015-12-04 19:12:45 +0530

    Input string added , reading function tested.

--- StringSearchAlgos/src/com/personal/StringSearchMainClass.java
+++ StringSearchAlgos/src/com/personal/StringSearchMainClass.java
@@ -6,8 +6,11 @@ public class StringSearchMainClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-
+		String location= "C:/Users/jyadav/git/String-Search-Algos/StringSearchAlgos/resources/inputFile.properties";
+		ReadInputFile readIn = new ReadInputFile();
+		String result= readIn.FileReadMethod(location);
+		System.out.println("The string is "+result);
+		System.out.println("String's length "+result.length());
 	}
 
 }
