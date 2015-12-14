bac3ed0f7266ac2b79abdac36ae5f21334f340c5
jy2054050 <jy2054050@gmail.com>
2015-12-14 13:48:27 +0530

    code formatting done and Rabin-Karp Method class added.

--- StringSearchAlgos/src/com/personal/NaiveSearch.java
+++ StringSearchAlgos/src/com/personal/NaiveSearch.java
@@ -1,26 +1,22 @@
 package com.personal;
 
 public class NaiveSearch {
-	
+	// Complexity O(n^2)
 	public int NaiveSearchResult(String pattern, String source){
 			int occured=0;
 			int patternLength = pattern.length();
 			int sourceLength = source.length();
-			//System.out.println(sourceLength + "  "+patternLength);
+			
 			for (int i = 0; i <= sourceLength - patternLength +1; i++){
 			        int j;
-			        /* For current index i, check for pattern match */
 			        for (j = 0; j < patternLength; j++)
 			            if (source.charAt(i+j) != pattern.charAt(j))
 			                break;
 			 
 			        if (j == patternLength) {
-			        //	System.out.println("Index : "+ i +" String is :"+ source.charAt(i-1) + source.charAt(i)+source.charAt(i+1));
 			        	occured++;
-			        }
-			      
+			       }
 			    }
-			 //System.out.println("asda " +occured);
 			return occured;
 			}
 	}
---
+++ StringSearchAlgos/src/com/personal/RabinCarpMethod.java
@@ -0,0 +1,5 @@
+package com.personal;
+
+public class RabinCarpMethod {
+//Rabin-Karp Hashed strings search method
+}
